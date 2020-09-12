4925b37428420aae7bde31c524090bd31f8232b9
Enzo Arauco <enzoarauco@gmail.com>
2020-09-12 12:56:08 -0500

    minor changes

--- src/main/kotlin/com/ac/ac/controller/CountryController.kt
+++ src/main/kotlin/com/ac/ac/controller/CountryController.kt
@@ -17,35 +17,35 @@ class CountryController constructor(
         private val countryService: CountryService){
 
     @GetMapping
-    fun findAll(): ResponseEntity<List<Country>> {
+    private fun findAll(): ResponseEntity<List<Country>> {
         val countries: List<Country> = this.countryService.findAll()
 
         return ResponseEntity(countries, HttpStatus.OK)
     }
 
     @PostMapping(consumes = ["application/json"], produces = ["application/json"])
-    fun addCountry(@RequestBody country: @Valid Country): ResponseEntity<Country> {
+    private fun addCountry(@RequestBody country: @Valid Country): ResponseEntity<Country> {
         val country1: Country = this.countryService.addCountry(country)
 
         return ResponseEntity(country1, HttpStatus.CREATED)
     }
 
     @GetMapping("{idCountry}", produces = ["application/json"])
-    fun findCountry(@PathVariable idCountry: Int): ResponseEntity<Optional<Country>>{
+    private fun findCountry(@PathVariable idCountry: Int): ResponseEntity<Optional<Country>>{
         val country: Optional<Country> = this.countryService.findById(idCountry)
 
         return ResponseEntity(country, HttpStatus.OK)
     }
 
     @PutMapping(produces = ["application/json"], consumes = ["application/json"])
-    fun updateCountry(@RequestBody country: @Valid Country): ResponseEntity<Country> {
+    private fun updateCountry(@RequestBody country: @Valid Country): ResponseEntity<Country> {
         val country1: Country = this.countryService.updateCountry(country)
 
         return ResponseEntity(country1, HttpStatus.ACCEPTED)
     }
 
     @DeleteMapping("{idCountry}")
-    fun deleteCountry(@PathVariable idCountry: Int): ResponseEntity<Unit>{
+    private fun deleteCountry(@PathVariable idCountry: Int): ResponseEntity<Unit>{
         val country: Unit = this.countryService.deleteCountry(idCountry)
 
         return ResponseEntity(country, HttpStatus.OK)
--- src/main/kotlin/com/ac/ac/controller/WeaponController.kt
+++ src/main/kotlin/com/ac/ac/controller/WeaponController.kt
@@ -17,35 +17,35 @@ class WeaponController constructor(
         private val weaponService: WeaponService){
 
     @GetMapping(produces = ["application/json"])
-    fun findAll(): ResponseEntity<List<Weapon>>{
+    private fun findAll(): ResponseEntity<List<Weapon>>{
         val weapon: List<Weapon> = this.weaponService.findAll()
 
         return ResponseEntity(weapon, HttpStatus.OK)
     }
 
     @PostMapping(produces = ["application/json"], consumes = ["application/json"])
-    fun addWeapon(@RequestBody @Valid weapon: Weapon): ResponseEntity<Weapon>{
+    private fun addWeapon(@RequestBody @Valid weapon: Weapon): ResponseEntity<Weapon>{
         val weapon1: Weapon = this.weaponService.addWeapon(weapon)
 
         return ResponseEntity(weapon1, HttpStatus.CREATED)
     }
 
     @GetMapping("{idWeapon}", produces = ["application/json"])
-    fun findWeapon(@PathVariable idWeapon: Int): ResponseEntity<Optional<Weapon>>{
+    private fun findWeapon(@PathVariable idWeapon: Int): ResponseEntity<Optional<Weapon>>{
         val weapon: Optional<Weapon> = this.weaponService.findById(idWeapon)
 
         return ResponseEntity(weapon, HttpStatus.OK)
     }
 
     @PutMapping(produces = ["application/json"], consumes = ["application/json"])
-    fun updateWeapon(@RequestBody @Valid weapon: Weapon): ResponseEntity<Weapon>{
+    private fun updateWeapon(@RequestBody @Valid weapon: Weapon): ResponseEntity<Weapon>{
         val weapon1: Weapon = this.weaponService.updateWeapon(weapon)
 
         return ResponseEntity(weapon1, HttpStatus.ACCEPTED)
     }
 
     @DeleteMapping("{idWeapon}", produces = ["application/json"])
-    fun deleteWeapon(@PathVariable idWeapon: Int) {
+    private fun deleteWeapon(@PathVariable idWeapon: Int) {
 
         this.weaponService.deleteWeapon(idWeapon)
     }
